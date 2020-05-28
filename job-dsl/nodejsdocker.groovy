4b42d54e9af8fc647b739fedee838e5712bfd862
DL <deleon@rocketmail.com>
2020-05-28 17:37:28 +0800

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,4 +1,4 @@
-job('NodeJS Docker example') {
+job('NodeJSDemo-PushDocker-Groovy') {
     scm {
         git('git://github.com/deleonchen/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
